3e61b56edc01f7b521001cd127d6cc3d514f5a6b
Khushipawar37 <khushipawar987@gmail.com>
2026-01-18 21:40:26 +0530

    LSTM GRU implementation done

--- rnn/main.py
+++ rnn/main.py
@@ -1,9 +1,9 @@
-# Step 1: Import Libraries and Load the Model
+# Step 1: Import Libraries and Load the Model   
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
-from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model   
 
 # Load the IMDB dataset word index
 word_index = imdb.get_word_index()
@@ -11,7 +11,7 @@ reverse_word_index = {value: key for key, value in word_index.items()}
 
 # Load the pre-trained model with ReLU activation
 
-model = load_model("simple_rnn_imdb_fixed.keras")
+model = load_model("simple_rnn_imdb_fixed.keras")  
 
 # Step 2: Helper Functions
 # Function to decode reviews
