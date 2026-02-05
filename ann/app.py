51e8b7e7a9bb66b103a4ac12aa92745584bf915d
Khushipawar37 <khushipawar987@gmail.com>
2026-02-05 22:56:14 +0530

    Streamlit app created

--- ann/app.py
+++ ann/app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import numpy as np
-import tensorflow as tf
+import numpy as np 
+import tensorflow as tf 
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
-
-# Load the trained model
+  
+# Load the trained model  
 model = tf.keras.models.load_model("model.h5", compile=False)
 
 # Load the encoders and scaler
